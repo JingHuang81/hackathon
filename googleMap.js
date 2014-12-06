6e727f8e3127e89da6054e4eaf8a664be6914dae
JingHuang81 <jennyhuangjing@hotmail.com>
2014-12-06 15:01:03 -0600

    Update googleMap.js

--- googleMap.js
+++ googleMap.js
@@ -2,7 +2,7 @@ var myLatlng = new google.maps.LatLng(-34.397, 150.644);
 var mapOptions = {
   zoom: 8,
   center: myLatlng,
-  mapTypeId: google.maps.MapTypeId.SATELLITE
+  mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 var map = new google.maps.Map(document.getElementById("map-canvas"),
     mapOptions);
